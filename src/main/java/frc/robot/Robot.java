84542dd419708ec2d4791954877b8e589e8a823e
Awesomeplayer165 <dev@jacob2.dev>
2024-07-09 15:51:04 -0700

    untested 8033 codebase migration with minimal logging usage

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,110 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.swerve.GyroIOPigeon2;
+import frc.robot.subsystems.swerve.SwerveSubsystem;
+import frc.robot.utils.CommandXboxControllerSubsystem;
+
+public class Robot extends TimedRobot {
+  public static enum RobotMode {
+    SIM,
+    REPLAY,
+    REAL
+  }
+
+  private Command autonomousCommand;
+
+  private RobotContainer robotContainer;
+
+  private final CommandXboxControllerSubsystem controller = new CommandXboxControllerSubsystem(0);
+  public static final RobotMode mode = Robot.isReal() ? RobotMode.REAL : RobotMode.REPLAY;
+
+  private final SwerveSubsystem swerve =
+      new SwerveSubsystem(
+          new GyroIOPigeon2(),
+          SwerveSubsystem.createTalonFXModules()
+      );
+
+  @Override
+  public void robotInit() {
+    robotContainer = new RobotContainer();
+
+    swerve.setDefaultCommand(
+        swerve.runVoltageTeleopFieldRelative(
+            () ->
+                new ChassisSpeeds(
+                    -teleopAxisAdjustment(controller.getLeftY()) * SwerveSubsystem.MAX_LINEAR_SPEED,
+                    -teleopAxisAdjustment(controller.getLeftX()) * SwerveSubsystem.MAX_LINEAR_SPEED,
+                    -teleopAxisAdjustment(controller.getRightX())
+                        * SwerveSubsystem.MAX_ANGULAR_SPEED)));
+  }
+
+  @Override
+  public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
+  }
+
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  @Override
+  public void autonomousInit() {
+    autonomousCommand = Commands.none();
+
+    // schedule the autonomous command (example)
+    if (autonomousCommand != null) {
+      autonomousCommand.schedule();
+    }
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {
+    if (autonomousCommand != null) {
+      autonomousCommand.cancel();
+    }
+  }
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void testInit() {
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
+
+  /** Modifies the given joystick axis value to make teleop driving smoother. */
+  private static double teleopAxisAdjustment(double x) {
+    return MathUtil.applyDeadband(Math.abs(Math.pow(x, 2)) * Math.signum(x), 0.02);
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/swerve/PhoenixOdometryThread.java
@@ -0,0 +1,163 @@
+// Copyright 2021-2024 FRC 6328, FRC 8033, Kevin Clark
+// http://github.com/Mechanical-Advantage
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 3 as published by the Free Software Foundation or
+// available in the root directory of this project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+package frc.robot.subsystems.swerve;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.hardware.ParentDevice;
+import com.google.common.collect.EvictingQueue;
+import com.google.common.collect.Sets;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.stream.Collectors;
+
+import org.littletonrobotics.junction.Logger;
+
+/**
+ * Provides an interface for high frequency sampling of Phoenix devices.
+ *
+ * <p>This version is intended for Phoenix 6 devices on both the RIO and CANivore buses. CAN FD
+ * compliant devices are required (TalonFX, CANCoder, Pigeon 2.0, CANdle) due to the use of the
+ * "waitForAll" blocking method to enable more consistent sampling. This also allows Phoenix Pro
+ * users to benefit from lower latency between devices using CANivore time synchronization.
+ */
+public class PhoenixOdometryThread extends Thread {
+  public record Registration(ParentDevice device, Set<StatusSignal<Double>> signals) {}
+
+  public record Samples(double timestamp, Map<StatusSignal<Double>, Double> values) {}
+
+  private final ReadWriteLock journalLock = new ReentrantReadWriteLock(true);
+
+  private final Set<StatusSignal<Double>> signals = Sets.newHashSet();
+  private final Queue<Samples> journal;
+
+  private static PhoenixOdometryThread instance = null;
+
+  public static PhoenixOdometryThread getInstance() {
+    if (instance == null) {
+      instance = new PhoenixOdometryThread();
+    }
+    return instance;
+  }
+
+  // Used for testing
+  protected static PhoenixOdometryThread createWithJournal(final Queue<Samples> journal) {
+    return new PhoenixOdometryThread(journal);
+  }
+
+  private PhoenixOdometryThread(final Queue<Samples> journal) {
+    setName("PhoenixOdometryThread");
+    setDaemon(true);
+
+    this.journal = journal;
+  }
+
+  private PhoenixOdometryThread() {
+    this(EvictingQueue.create(20));
+  }
+
+  public void registerSignals(Collection<Registration> registrations) {
+    registerSignals(registrations.toArray(new Registration[] {}));
+  }
+
+  // Returns a handle which can be used to collect the last 20 signal results
+  public void registerSignals(Registration... registrations) {
+    var writeLock = journalLock.writeLock();
+
+    try {
+      writeLock.lock();
+
+      for (var registration : registrations) {
+        assert CANBus.isNetworkFD(registration.device.getNetwork()) : "Only CAN FDs supported";
+
+        signals.addAll(registration.signals);
+      }
+    } finally {
+      writeLock.unlock();
+    }
+  }
+
+  public List<Samples> samplesSince(double timestamp, Set<StatusSignal<Double>> signals) {
+    var readLock = journalLock.readLock();
+    try {
+      readLock.lock();
+
+      return journal.stream()
+          .filter(s -> s.timestamp > timestamp)
+          .map(
+              s -> {
+                var filteredValues =
+                    s.values.entrySet().stream()
+                        .filter(e -> signals.contains(e.getKey()))
+                        .collect(
+                            Collectors.toUnmodifiableMap(Map.Entry::getKey, Map.Entry::getValue));
+                return new Samples(s.timestamp, filteredValues);
+              })
+          .collect(Collectors.toUnmodifiableList());
+    } finally {
+      readLock.unlock();
+    }
+  }
+
+  public List<Samples> samplesSince(double timestamp) {
+    return samplesSince(timestamp, signals);
+  }
+
+  @Override
+  public void run() {
+    while (true) {
+      // Wait for updates from all signals
+      var writeLock = journalLock.writeLock();
+      // NOTE (kevinclark): The toArray here in a tight loop is kind of ugly
+      // but keeping up a symmetric array is too and it's probably negligible on latency.
+      BaseStatusSignal.waitForAll(
+          2.0 / Module.ODOMETRY_FREQUENCY_HZ, signals.toArray(new BaseStatusSignal[0]));
+      try {
+        writeLock.lock();
+        var filteredSignals =
+            signals.stream()
+                .filter(s -> s.getStatus().equals(StatusCode.OK))
+                .collect(Collectors.toSet());
+        journal.add(
+            new Samples(
+                timestampFor(filteredSignals),
+                filteredSignals.stream()
+                    .collect(Collectors.toUnmodifiableMap(s -> s, s -> s.getValueAsDouble()))));
+      } finally {
+        writeLock.unlock();
+      }
+    }
+  }
+
+  private double timestampFor(Set<StatusSignal<Double>> signals) {
+    double timestamp = Logger.getRealTimestamp() / 1e6;
+
+    final double totalLatency =
+        signals.stream().mapToDouble(s -> s.getTimestamp().getLatency()).sum();
+
+    // Account for mean latency for a "good enough" timestamp
+    if (!signals.isEmpty()) {
+      timestamp -= totalLatency / signals.size();
+    }
+
+    return timestamp;
+  }
+}
