2cd0433f51b4042462094d68e183dfaea3afd6ce
Ishan Venkatesh <242120@students.srvusd.net>
2024-09-30 16:59:38 -0700

    Adding simulation files

--- 1458TeamCode/src/main/java/frc/robot/subsystems/Drive.java
+++ 1458TeamCode/src/main/java/frc/robot/subsystems/Drive.java
@@ -8,13 +8,28 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 
+import java.util.List;
+
 import com.ctre.phoenix6.configs.Pigeon2Configuration;
 import com.ctre.phoenix6.hardware.Pigeon2;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain;
+import com.ctre.phoenix6.sim.Pigeon2SimState;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.simulation.AnalogGyroSim;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
+import edu.wpi.first.wpilibj.simulation.EncoderSim;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -22,6 +37,16 @@ public class Drive extends SubsystemBase {
     public SwerveDriveOdometry swerveOdometry;
     public Module[] mSwerveMods;
     public Pigeon2 gyro;
+
+    private Trajectory trajectory;
+    private final Field2d field = new Field2d();
+    private Pose2d simPose = new Pose2d();
+
+    // // Simulation
+    // private final AnalogGyroSim m_gyroSim = new AnalogGyroSim(m_gyro);
+    // private final EncoderSim m_leftEncoderSim = new EncoderSim(m_leftEncoder);
+    // private final EncoderSim m_rightEncoderSim = new EncoderSim(m_rightEncoder);
+    private final Pigeon2SimState gyroSim;
 
     public Drive() {
         gyro = new Pigeon2(Constants.Swerve.pigeonID, "CV");
@@ -35,8 +60,25 @@ public class Drive extends SubsystemBase {
                 new Module(3, Constants.Swerve.BackRightMod.constants)
         };
 
-
         swerveOdometry = new SwerveDriveOdometry(Constants.Swerve.swerveKinematics, getGyroYaw(), getModulePositions());
+
+        // Simulation
+        // Create and push Field2d to SmartDashboard.
+        SmartDashboard.putData("Field", field);
+
+        // Create the trajectory to follow in autonomous.
+        trajectory =
+            TrajectoryGenerator.generateTrajectory(
+                new Pose2d(0, 0, Rotation2d.fromDegrees(0)),
+                List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+                new Pose2d(3, 0, Rotation2d.fromDegrees(0)),
+                new TrajectoryConfig(Units.feetToMeters(3.0), Units.feetToMeters(3.0))
+            );
+
+        // Push the trajectory to Field2d.
+        field.getObject("traj").setTrajectory(trajectory);
+
+        gyroSim = gyro.getSimState();
     }
 
     public void drive(Translation2d translation, double rotation, boolean fieldRelative, boolean isOpenLoop) {
@@ -127,10 +169,39 @@ public class Drive extends SubsystemBase {
         for (Module mod : mSwerveMods) {
             SmartDashboard.putNumber("Mod " + mod.moduleNumber + " CANcoder", (mod.getCANcoder().getDegrees()+180)%360);
 
-
 //            SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Angle", ((mod.mAngleMotor.getPosition().getValue()%22.0)*360/22-180));
             SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Angle", ((mod.mAngleMotor.getPosition().getValue() * 360) % 360 + 360) % 360); // This is super specific, don't break this pls
 //            SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Velocity", mod.getState().speedMetersPerSecond);
         }
+
+        field.setRobotPose(swerveOdometry.getPoseMeters());
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        // TODO: Sim jointly as one system
+        for (Module mod : mSwerveMods) {
+            mod.updateSimPeriodic();
+
+            SmartDashboard.putNumber("Mod " + mod.moduleNumber + " CANcoder", (mod.getCANcoder().getDegrees()+180)%360);
+            SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Angle", ((mod.mAngleMotor.getPosition().getValue() * 360) % 360 + 360) % 360); // This is super specific, don't break this pls
+        }
+
+        // final ChassisSpeeds chassisSpeeds = Constants.Swerve.swerveKinematics.toChassisSpeeds(getModuleStates());
+        // simPose = simPose.transformBy((
+        //     new Transform2d(
+        //         new Translation2d(
+        //             chassisSpeeds.vxMetersPerSecond * TimedRobot.kDefaultPeriod * 100000,
+        //             chassisSpeeds.vyMetersPerSecond * TimedRobot.kDefaultPeriod * 100000),
+        //         // new Translation2d(
+        //         //     20,
+        //         //     20),
+        //         new Rotation2d(chassisSpeeds.omegaRadiansPerSecond * TimedRobot.kDefaultPeriod)
+        //     )
+        // ));
+        // field.setRobotPose(simPose);
+
+        swerveOdometry.update(getGyroYaw(), getModulePositions());
+        field.setRobotPose(swerveOdometry.getPoseMeters());
     }
 }
