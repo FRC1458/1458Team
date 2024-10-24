d362fb1dc367c0c8e9f7d9007d6eee016cc27796
dcao6668 <dcao6668@gmail.com>
2024-10-23 23:27:31 -0700

    rewrite the TrajectoryIterator class based on wpilib Trajectory package

---
+++ 1458TeamCode/src/main/java/frc/robot/lib/trajectory/TrajectoryIterator.java
@@ -0,0 +1,46 @@
+package frc.robot.lib.trajectory;
+
+import edu.wpi.first.math.trajectory.*;
+
+//dc.10.21.2024, rewrite the TrajectoryIterator class based on wpilib Trajectory package, main functions as following
+//
+//2. rewrite the advance () method which is used often by its callers
+//
+public class TrajectoryIterator {
+    protected double progress_ = 0.0;
+    protected Trajectory.State current_sample_;     //corresponding to TrajectorySamplePoint in citrus code
+    protected Trajectory mCurrentTrajectory=null;
+
+    //construtor code
+    public TrajectoryIterator (Trajectory curTrajectory){
+        mCurrentTrajectory=curTrajectory;
+    }
+
+    //preview the trajectory
+    public Trajectory.State advance (double additional_progress){
+        progress_ = Math.max(0.0, Math.min(mCurrentTrajectory.getTotalTimeSeconds(), progress_ + additional_progress));
+        current_sample_ = mCurrentTrajectory.sample(progress_);
+        return current_sample_;
+    }
+
+    //preview the trajectory
+    public Trajectory.State preview (double additional_progress){
+        final double progress = Math.max(0.0, Math.min(mCurrentTrajectory.getTotalTimeSeconds(), progress_ + additional_progress));
+        return mCurrentTrajectory.sample(progress);
+    }
+
+    public boolean isDone() {
+        return getRemainingProgress() == 0.0;
+    }
+
+    public double getProgress() {
+        return progress_;
+    }
+
+    public double getRemainingProgress() {
+        return Math.max(0.0, mCurrentTrajectory.getTotalTimeSeconds() - progress_);
+    }    
+
+    //access to the current trajectory properties
+    public Trajectory trajectory(){ return mCurrentTrajectory;}
+}
