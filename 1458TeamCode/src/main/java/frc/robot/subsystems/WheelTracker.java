25393b3fa5a3809a6f992aefca0271a9a7ace52c
dcao6668 <dcao6668@gmail.com>
2024-10-26 11:32:58 -0700

    add a few dummy methods in order to compile

--- 1458TeamCode/src/main/java/frc/robot/subsystems/WheelTracker.java
+++ 1458TeamCode/src/main/java/frc/robot/subsystems/WheelTracker.java
@@ -1,6 +1,14 @@
 package frc.robot.subsystems;
 
-//dc.10.21.2024 dummy class as placeholder, To be implemented. 
+import edu.wpi.first.math.geometry.Pose2d;
+import frc.robot.lib.swerve.SwerveModule;
+
+//dc.10.21.2024 TODO: a dummy WheelTracker class to pass compilation, placeholder for actual implementation
 public class WheelTracker {
+    //TODO: WheelTracker entire class needs to be implemented
+    public WheelTracker(SwerveModule[] modules) {}
+	public synchronized Pose2d getRobotPose() {	return null;}
+    public void stop() {}
+
     
 }
