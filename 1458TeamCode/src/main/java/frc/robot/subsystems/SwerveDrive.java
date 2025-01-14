0bad6d7da62286a339f55e0f56c4c35ca92a6ef6
Tommy Cao <tomcao36@gmail.com>
2025-01-13 18:40:40 -0800

    j

--- 1458TeamCode/src/main/java/frc/robot/subsystems/SwerveDrive.java
+++ 1458TeamCode/src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -1,6 +1,11 @@
 package frc.robot.subsystems;
 
-//dc.10.25.2024, ported from com.team1678.frc2024.subsystems.Drive;
+//dc.10.25.2024, ported from com.team1678.frc2024.subsystems.Drive
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+import com.pathplanner.lib.controllers.PathFollowingController;
 
 import frc.robot.Constants;
 import frc.robot.Robot;
@@ -44,6 +49,7 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -112,6 +118,16 @@ public class SwerveDrive extends Subsystem {
 		mWheelTracker = new WheelTracker(mModules);
 
 		SmartDashboard.putData("Field", m_field);
+	
+	}
+
+	private ChassisSpeeds getRobotRelativeSpeeds() {
+		return new ChassisSpeeds();
+	}
+
+	private Object driveRobotRelative(ChassisSpeeds speeds) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'driveRobotRelative'");
 	}
 
 	public void setKinematicLimits(KinematicLimits newLimits) {
